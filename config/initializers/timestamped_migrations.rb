df7d2b8cddbeb1a731d52e845995bbffeed596f0
ArthurYaxiongWang <arthurwyx@gmail.com>
2024-07-03 22:03:55 -0500

    Set up the sorcery

---
+++ config/initializers/timestamped_migrations.rb
@@ -0,0 +1,15 @@
+module TimestampedMigrations
+  def timestamped_migrations
+    # Example logic for managing timestamped migrations
+    # You can customize this to fit your needs
+
+    # Here, we're just printing a message to the console
+    puts "Managing timestamped migrations"
+
+    # You can add any additional logic needed here
+  end
+end
+
+ActiveSupport.on_load(:active_record) do
+  ActiveRecord::Base.extend(TimestampedMigrations)
+end
