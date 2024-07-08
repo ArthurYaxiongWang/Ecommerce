0425972bee17dd59ba5561129aeacebb08d39bea
ArthurYaxiongWang <arthurwyx@gmail.com>
2024-07-07 20:06:54 -0500

    Done with the admin product index page setting

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,50 @@
+class Admin::ProductsController < Admin::BaseController
+  before_action :find_product, only: [:edit, :update, :destroy]
+
+  def index
+    @products = Product.page(params[:page] || 1).per_page(params[:per_page] || 10).order(id: "desc")
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(params.require(:product).permit!)
+    if @product.save
+      flash[:notice] = "Product created successfully!"
+      redirect_to admin_products_path
+    else
+      render action: :new
+    end
+  end
+
+  def edit
+    render action: :new
+  end
+
+  def update
+    @product.attributes = params.require(:product).permit!
+    if @product.save
+      flash[:notice] = "Product updated successfully!"
+      redirect_to admin_products_path
+    else
+      render action: :new
+    end
+  end
+
+  def destroy
+    if @product.destroy
+      flash[:notice] = "Product deleted successfully!"
+      redirect_to admin_products_path
+    else
+      flash[:notice] = "Product can't be deleted!"
+      redirect_to :back
+    end
+  end
+
+  private
+  def find_product
+    @product = Product.find(params[:id])
+  end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,18 @@
 class Product < ApplicationRecord
+  validates :category_id, presence: { message: "Category can't be blank" }
+  validates :title, presence: { message: "Title can't be blank" }
+  validates :status, inclusion: { in: ["on", "off"] , message: "Status must be on or off" }
+  validates :amount, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: "Amount must be an integer greater than or equal to 0" }
+  validates :amount, presence: { message: "Amount can't be blank" },
+    if: proc { |product| !product.amount.blank?}
+  validates :msrp, presence: { message: "MSRP can't be blank" }
+  validates :msrp, numericality: { greater_than_or_equal_to: 0, message: "MSRP must be greater than or equal to 0" },
+    if: proc { |product| !product.msrp.blank?}
+  validates :price, presence: { message: "Price can't be blank" }
+  validates :price, numericality: { greater_than_or_equal_to: 0, message: "Price must be greater than or equal to 0" },
+    if: proc { |product| !product.price.blank?}
+  validates :description, presence: { message: "Description can't be blank" }
+
   belongs_to :category
 
   before_create :set_default_attrs
