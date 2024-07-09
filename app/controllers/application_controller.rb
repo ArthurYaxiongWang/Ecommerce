44cfe20620c90b6fda9cb585fb40749033f7f9bf
ArthurYaxiongWang <arthurwyx@gmail.com>
2024-07-09 16:51:36 -0500

    Fixed minor bugs with category display

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :fetch_home_data
+
   protected
   def fetch_home_data
     @categories = Category.grouped_data
