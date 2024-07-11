baf4f5d245e1d61be4b0160ab74f0b2c0a76cdb4
ArthurYaxiongWang <arthurwyx@gmail.com>
2024-07-10 19:52:03 -0500

    Done with add_to_cart button function

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -5,5 +5,17 @@ class ShoppingCartsController < ApplicationController
     @shopping_carts = ShoppingCart.by_user_uuid(session[:user_uuid]).order(id: "desc").includes(:product => [:main_product_image])
   end
 
+  def create
+    amount = params[:amount].to_i
+    amount = amount <= 0 ? 1 : amount
 
+    @product = Product.find(params[:product_id])
+    @shopping_cart = ShoppingCart.create_or_update({
+      user_uuid: session[:user_uuid],
+      product_id: params[:product_id],
+      amount: amount
+    })
+
+    render layout: false
+  end
 end
--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -6,4 +6,20 @@ class ShoppingCart < ApplicationRecord
   belongs_to :product
 
   scope :by_user_uuid, ->(user_uuid) { where(user_uuid: user_uuid) }
+
+  def self.create_or_update options = {}
+    cond = {
+      user_uuid: options[:user_uuid],
+      product_id: options[:product_id]
+    }
+
+    record = where(cond).first
+    if record
+      record.update_attributes!(options.merge(amount: cart.amount + options[:amount]))
+    else
+      record = create!(options)
+    end
+
+    record
+  end
 end
