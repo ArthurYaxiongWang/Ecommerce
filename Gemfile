752b2dd7e09ebb1476b7251e795a9d2128863d31
ArthurYaxiongWang <arthurwyx@gmail.com>
2024-07-13 14:38:18 -0500

    added the webpacker gem but didn't fix the remove error

--- Gemfile
+++ Gemfile
@@ -87,3 +87,5 @@ gem 'faker', '~> 2.15', '>= 2.15.1'
 gem 'rails-ujs', '~> 0.1.0'
 
 gem 'devise', '~> 4.8', '>= 4.8.1'
+
+gem  'webpacker', '~> 5.4', '>= 5.4.3'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,3 +19,14 @@
 //= require bootstrap-sprockets
 
 //= require home
+
+// app/javascript/packs/application.js
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
