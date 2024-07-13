245737e3303908eb3aa760c9785cd7b7b4eaa174
ArthurYaxiongWang <arthurwyx@gmail.com>
2024-07-13 17:26:53 -0500

    User sign up page with address and province info is done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_13_181656) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_13_204905) do
   create_table "abouts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -93,6 +93,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_13_181656) do
     t.index ["uuid"], name: "index_products_on_uuid"
   end
 
+  create_table "provinces", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "shopping_carts", force: :cascade do |t|
     t.integer "user_id"
     t.string "user_uuid"
@@ -123,8 +129,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_13_181656) do
     t.string "encrypted_password", default: "", null: false
     t.datetime "reset_password_sent_at", precision: nil
     t.datetime "remember_created_at", precision: nil
+    t.integer "province_id"
+    t.string "address"
     t.index ["activation_token"], name: "index_users_on_activation_token"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["province_id"], name: "index_users_on_province_id"
     t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
     t.index ["uuid"], name: "index_users_on_uuid", unique: true
@@ -132,4 +141,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_13_181656) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "users", "provinces"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,42 @@
 require 'faker'
 
-# Clear existing data
 Product.destroy_all
 Category.destroy_all
+Province.destroy_all
+User.destroy_all
 
-# Create a directory path for the images
 image_path = Rails.root.join('db', 'seeds', 'images', 'default_image.png')
 
-# Create parent categories
 parent_categories = []
 2.times do |i|
   parent_categories << Category.create!(title: "Parent Category #{i + 1}")
 end
 
-# Create subcategories with parent categories
 categories = []
 4.times do |i|
   categories << Category.create!(title: "Category #{i + 1}", parent: parent_categories.sample)
 end
 
-# Create 100 products, distributed across the 4 categories
+provinces = [
+  { name: 'Alberta' },
+  { name: 'British Columbia' },
+  { name: 'Manitoba' },
+  { name: 'New Brunswick' },
+  { name: 'Newfoundland and Labrador' },
+  { name: 'Nova Scotia' },
+  { name: 'Ontario' },
+  { name: 'Prince Edward Island' },
+  { name: 'Quebec' },
+  { name: 'Saskatchewan' },
+  { name: 'Northwest Territories' },
+  { name: 'Nunavut' },
+  { name: 'Yukon' }
+]
+
+provinces.each do |province|
+  Province.create!(province)
+end
+
 100.times do
   product = Product.create!(
     title: Faker::Commerce.product_name,
@@ -29,10 +46,9 @@ end
     amount: Faker::Number.between(from: 1, to: 100),
     uuid: Faker::Number.unique.number(digits: 10),
     category: categories.sample,
-    status: 'on' # Ensure status is set to 'on'
+    status: 'on'
   )
 
-  # Attach an image to the product using ActiveStorage
   if File.exist?(image_path)
     product.image.attach(io: File.open(image_path), filename: 'default_image.png')
   else
@@ -40,5 +56,17 @@ end
   end
 end
 
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: 'password',
+    password_confirmation: 'password',
+    address: Faker::Address.street_address,
+    province: Province.all.sample
+  )
+end
+
 puts "Created #{Category.count} categories"
 puts "Created #{Product.count} products"
+puts "Created #{Province.count} provinces"
+puts "Created #{User.count} users"
