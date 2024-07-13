9a21d23c216056abb867907d5900619a0c6e9e2c
ArthurYaxiongWang <arthurwyx@gmail.com>
2024-07-13 18:14:24 -0500

    Changed the scss structure to meet the requirement

--- app/javascript/cable.js
+++ app/javascript/cable.js
@@ -1,13 +1,7 @@
-// Action Cable provides the framework to deal with WebSockets in Rails.
-// You can generate new channels where WebSocket features live using the rails generate channel command.
-//
-//= require action_cable
-//= require_self
-//= require_tree ./channels
+// app/javascript/cable.js
 
-(function() {
-  this.App || (this.App = {});
+import { createConsumer } from "@rails/actioncable"
 
-  App.cable = ActionCable.createConsumer();
+const consumer = createConsumer()
 
-}).call(this);
+export default consumer
