c5bd3e1a53e1e14719d1a7c71c842d4d928b0e03
ArthurYaxiongWang <arthurwyx@gmail.com>
2024-07-14 19:24:50 -0500

    Search function initially done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,23 @@
 class ProductsController < ApplicationController
+  before_action :fetch_home_data
+
   def index
     @products = Product.page(params[:page]).per(9)
   end
 
   def show
-    fetch_home_data
-
     @product = Product.find(params[:id])
+  end
+
+  def search
+    @products = Product.where("title like :title", title: "%#{params[:w]}%")
+    .order("id desc").page(params[:page] || 1).per(params[:per_page] || 9)
+    .includes(:main_product_image)
+
+    unless params[:category_id].blank?
+      @products = @products.where(category_id: params[:category_id])
+    end
+
+    render 'welcome/index'
   end
 end
