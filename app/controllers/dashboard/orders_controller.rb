2fde53415b59f2eb2bd1ad7f498b65f30669fe4f
ArthurYaxiongWang <arthurwyx@gmail.com>
2024-07-15 20:06:15 -0500

    Views are OK but no orders created

--- app/controllers/dashboard/orders_controller.rb
+++ app/controllers/dashboard/orders_controller.rb
@@ -1,4 +1,8 @@
-class Dashboard::OrdersController < Dashboard::BaseController
+class Dashboard::OrdersController < ApplicationController
+  before_action :authenticate_user
+
   def index
+    @orders = current_user.orders.page(params[:page] || 1).per(params[:per_page] || 10)
+      .includes([[:product => [:main_product_image]], :address]).order("id desc")
   end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,24 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user
+  before_action :set_addresses_and_cart, only: [:new, :create]
 
   def new
-    fetch_home_data
-    @addresses = current_user.addresses
-    @shopping_cart = ShoppingCart.by_user_uuid(session[:user_uuid]).includes(cart_items: :product).first
+    @order = current_user.orders.new
   end
 
   def create
     @order = current_user.orders.new(order_params)
-    shopping_carts = ShoppingCart.by_user_uuid(current_user.uuid)
 
     ActiveRecord::Base.transaction do
       if @order.save
-        shopping_carts.each do |cart_item|
+        @shopping_cart.cart_items.each do |cart_item|
           @order.order_items.create!(
             product: cart_item.product,
-            quantity: cart_item.amount,
-            total_price: cart_item.product.price * cart_item.amount
+            quantity: cart_item.quantity,
+            total_price: cart_item.product.price * cart_item.quantity
           )
         end
-        shopping_carts.destroy_all
+        @shopping_cart.destroy!
         redirect_to @order, notice: 'Order was successfully created.'
       else
         render :new
@@ -32,6 +30,11 @@ class OrdersController < ApplicationController
   end
 
   private
+
+  def set_addresses_and_cart
+    @addresses = current_user.addresses
+    @shopping_cart = ShoppingCart.by_user_uuid(session[:user_uuid]).includes(cart_items: :product).first
+  end
 
   def order_params
     params.require(:order).permit(:address_id, :total_price, :amount)
