b8efbf5783563c344b0f32afe4e50f92b44eb783
ArthurYaxiongWang <arthurwyx@gmail.com>
2024-07-21 07:03:56 -0500

    Minor view changes before presentation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     html_class += " #{options[:html_class]}" unless options[:html_class].blank?
 
     button_to shopping_carts_path, method: :post, params: { product_id: product.id, amount: 1 }, class: html_class do
-      "<i class='fa fa-spinner'></i> Add to cart".html_safe
+      "Add to cart".html_safe
     end
   end
 
